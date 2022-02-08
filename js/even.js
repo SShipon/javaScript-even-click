8eb9806a89e7914547a8967834a380f91ae6306c
SShipon <nxshipon@gmail.com>
2022-02-08 11:25:08 +0600

    hey

---
+++ js/even.js
@@ -0,0 +1,39 @@
+function makeRed() {
+    document.body.style.background='red'
+}
+
+//handle blue button click me
+const blueButton = document.getElementById("blue-button");
+//console.log('blue-button');
+blueButton.onclick = makeBlue;
+function makeBlue() {
+    document.body.style.backgroundColor='blue'
+}
+
+//handle event using anonymous function
+
+const greenButton = document.getElementById("make-green-button");
+greenButton.onclick = function() {
+    document.body.style.background='green'
+}
+
+const blackButton = document.getElementById("black-button");
+blackButton.addEventListener("click", makeBlack);
+function makeBlack() {
+     document.body.style.background='black'
+}
+ //addEventListener
+const hotPink = document.getElementById("hot-pink");
+hotPink.addEventListener('click', function makePink() {
+    document.body.style.background="hotpink"
+})
+console.log(hotPink);
+
+//direct shortcut
+document.getElementById("lightBlue").addEventListener('click', function () {
+    document.body.style.background='lightBlue'
+});
+
+document.getElementById("khaki").addEventListener("click", function () {
+  document.body.style.background = "khaki";
+});
